239d8f14a037d9d494bb2c9b9566cb4c0d193e96
Krishnadas <krishnadas100033@gmail.com>
2019-09-16 19:37:22 +0530

    First commit

---
+++ LightsOn.cs
@@ -0,0 +1,23 @@
+﻿// program to switch on the lights
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class lighton
+    {
+        static void Main(string[] args)
+        {
+            SerialPort sp = new SerialPort();
+            sp.PortName = "com4";
+            sp.BaudRate = 9600;
+            sp.Open(); 
+            sp.Write("d");
+            sp.Close();
+        }
+    }
+}
